a2f3cc13e9a33c17d6274738767c864f907e7844
OmeTiberiusKirk <nukool@40.co.th>
2025-09-30 18:09:18 +0700

    ~

---
+++ hints.py
@@ -0,0 +1,8 @@
+from typing import TypedDict
+
+
+class Config(TypedDict):
+    WEB_BASE_URL: str
+    EMAIL: str
+    PASS: str
+    MDX_BASE_URL: str
--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import requests, re
+from dotenv import dotenv_values
+import hints
 
 if __name__ == "__main__":
+    config: hints.Config = dotenv_values(".env")
+
     with requests.Session() as s:
         # Session automatically stores cookies for you
-        r = s.get("http://192.168.2.41")
+        r = s.get(config["WEB_BASE_URL"])
 
         # Extract _token hidden input
         m = re.search(r'name="_token".+value="([^"]+)"', r.text)
         token = m.group(1)
 
-        payload = {"_token": token, "email": "user@example.com", "password": "user123"}
+        payload = {
+            "_token": token,
+            "email": config["EMAIL"],
+            "password": config["PASS"],
+        }
 
         # POST using the same session (cookies are kept automatically)
-        r = s.post("http://192.168.2.41/auth", data=payload)
-        # r2 = s.get('http://192.168.2.41/fta-monitoring-country')
-    print(r)
+        r = s.post(f"{config['WEB_BASE_URL']}/auth", data=payload)
+        print(f"url: {r.url}")
+        print(f"status: {r.status_code}")
+
+        r = s.get(f"{config['WEB_BASE_URL']}/fta-monitoring-country")
+        print(f"url: {r.url}")
+        print(f"status: {r.status_code}")
+
